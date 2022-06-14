577d2e38fd2308abe4a6d6fa3153762c2ada3418
George Robinson <george.robinson@grafana.com>
2022-06-14 10:26:10 +0100

    Alerting: Add support for images in Kafka alerts

--- pkg/services/ngalert/notifier/channels/kafka_test.go
+++ pkg/services/ngalert/notifier/channels/kafka_test.go
@@ -16,6 +16,9 @@ import (
 func TestKafkaNotifier(t *testing.T) {
 	tmpl := templateForTests(t)
 
+	images, deleteFunc := newFakeImageStore(t)
+	defer deleteFunc()
+
 	externalURL, err := url.Parse("http://localhost")
 	require.NoError(t, err)
 	tmpl.ExternalURL = externalURL
@@ -29,7 +32,7 @@ func TestKafkaNotifier(t *testing.T) {
 		expMsgError    error
 	}{
 		{
-			name: "One alert",
+			name: "A single alert with image",
 			settings: `{
 				"kafkaRestProxy": "http://localhost",
 				"kafkaTopic": "sometopic"
@@ -38,7 +41,7 @@ func TestKafkaNotifier(t *testing.T) {
 				{
 					Alert: model.Alert{
 						Labels:      model.LabelSet{"alertname": "alert1", "lbl1": "val1"},
-						Annotations: model.LabelSet{"ann1": "annv1", "__dashboardUid__": "abcd", "__panelId__": "efgh"},
+						Annotations: model.LabelSet{"ann1": "annv1", "__dashboardUid__": "abcd", "__panelId__": "efgh", "__alertScreenshotToken__": "test-image"},
 					},
 				},
 			},
@@ -50,6 +53,7 @@ func TestKafkaNotifier(t *testing.T) {
 						"alert_state": "alerting",
 						"client": "Grafana",
 						"client_url": "http://localhost/alerting/list",
+						"contexts": [{"type": "image", "src": "https://www.example.com/test-image.jpg"}],
 						"description": "[FIRING:1]  (val1)",
 						"details": "**Firing**\n\nValue: [no value]\nLabels:\n - alertname = alert1\n - lbl1 = val1\nAnnotations:\n - ann1 = annv1\nSilence: http://localhost/alerting/silence/new?alertmanager=grafana&matcher=alertname%3Dalert1&matcher=lbl1%3Dval1\nDashboard: http://localhost/d/abcd\nPanel: http://localhost/d/abcd?viewPanel=efgh\n",
 						"incident_key": "6e3538104c14b583da237e9693b76debbc17f0f8058ef20492e5853096cf8733"
@@ -59,7 +63,7 @@ func TestKafkaNotifier(t *testing.T) {
 				}`,
 			expMsgError: nil,
 		}, {
-			name: "Multiple alerts",
+			name: "Multiple alerts with images",
 			settings: `{
 				"kafkaRestProxy": "http://localhost",
 				"kafkaTopic": "sometopic"
@@ -68,12 +72,12 @@ func TestKafkaNotifier(t *testing.T) {
 				{
 					Alert: model.Alert{
 						Labels:      model.LabelSet{"alertname": "alert1", "lbl1": "val1"},
-						Annotations: model.LabelSet{"ann1": "annv1"},
+						Annotations: model.LabelSet{"ann1": "annv1", "__alertScreenshotToken__": "test-image"},
 					},
 				}, {
 					Alert: model.Alert{
 						Labels:      model.LabelSet{"alertname": "alert1", "lbl1": "val2"},
-						Annotations: model.LabelSet{"ann1": "annv2"},
+						Annotations: model.LabelSet{"ann1": "annv2", "__alertScreenshotToken__": "test-image-2"},
 					},
 				},
 			},
@@ -85,6 +89,7 @@ func TestKafkaNotifier(t *testing.T) {
 						"alert_state": "alerting",
 						"client": "Grafana",
 						"client_url": "http://localhost/alerting/list",
+						"contexts": [{"type": "image", "src": "https://www.example.com/test-image.jpg"}],
 						"description": "[FIRING:2]  ",
 						"details": "**Firing**\n\nValue: [no value]\nLabels:\n - alertname = alert1\n - lbl1 = val1\nAnnotations:\n - ann1 = annv1\nSilence: http://localhost/alerting/silence/new?alertmanager=grafana&matcher=alertname%3Dalert1&matcher=lbl1%3Dval1\n\nValue: [no value]\nLabels:\n - alertname = alert1\n - lbl1 = val2\nAnnotations:\n - ann1 = annv2\nSilence: http://localhost/alerting/silence/new?alertmanager=grafana&matcher=alertname%3Dalert1&matcher=lbl1%3Dval2\n",
 						"incident_key": "6e3538104c14b583da237e9693b76debbc17f0f8058ef20492e5853096cf8733"
@@ -127,7 +132,7 @@ func TestKafkaNotifier(t *testing.T) {
 			ctx := notify.WithGroupKey(context.Background(), "alertname")
 			ctx = notify.WithGroupLabels(ctx, model.LabelSet{"alertname": ""})
 
-			pn := NewKafkaNotifier(cfg, webhookSender, tmpl)
+			pn := NewKafkaNotifier(cfg, images, webhookSender, tmpl)
 			ok, err := pn.Notify(ctx, c.alerts...)
 			if c.expMsgError != nil {
 				require.False(t, ok)
